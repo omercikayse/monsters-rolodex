af4291bea49b50ce34cb37c51f402b8e4a5cafef
Omercik, Ayse <ayse.omercik@accenture.com>
2019-12-12 16:50:07 +0300

    Updates

---
+++ precache-manifest.47306d37f1566c669dad8592e09d23b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914c278a85f7ca7e2dcfa6d76133bcdf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a137d3f866da2039aa30",
+    "url": "/monsters-rolodex/static/css/main.ebaac15e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a137d3f866da2039aa30",
+    "url": "/monsters-rolodex/static/js/main.67d37186.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
